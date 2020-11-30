62c8d2c3f8d986ca33b821165197c500297f275a
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-30 03:15:51 -0300

    'init'

---
+++ precache-manifest.4f4ab354f9a3b4d80ac0ea8b1809c2bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb1be4da5b059e3e131e881481ec9ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48855f7bfde26fc86d6",
+    "url": "/static/css/2.1e132f5d.chunk.css"
+  },
+  {
+    "revision": "b6f010227609aec59937",
+    "url": "/static/css/main.d704dc43.chunk.css"
+  },
+  {
+    "revision": "f48855f7bfde26fc86d6",
+    "url": "/static/js/2.9e7c9bd1.chunk.js"
+  },
+  {
+    "revision": "80d1cc6eb6ed8a2ef448d3355943f4f2",
+    "url": "/static/js/2.9e7c9bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f010227609aec59937",
+    "url": "/static/js/main.a2c9d1ab.chunk.js"
+  },
+  {
+    "revision": "3a8ff1ce20581170cb07",
+    "url": "/static/js/runtime-main.4dafe7ba.js"
+  }
+]);
